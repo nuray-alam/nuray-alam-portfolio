f39fcdaa08b55e47cacb0ac61b69f9c589f23d1e
nuray-alam <siddik4x@gmail.com>
2021-11-29 14:27:23 +0600

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Banner.css'
+const Banner = () => {
+    return (
+        <div>
+            <h2>This is banner</h2>
+        </div>
+    );
+};
+
+export default Banner;
