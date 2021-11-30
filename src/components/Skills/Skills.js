acfd8e52e82b47ac2404a9e9bf50a556657a38bb
nuray-alam <siddik4x@gmail.com>
2021-11-30 13:36:27 +0600

    margin fixes

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -9,35 +9,35 @@ const Skills = () => {
             <div className="" >
                 <h5 className="text-white">Frontend</h5>
                 <p className="text-white">
-                    <span className="me-3 btn btn-outline-primary">Javascript</span>
-                    <span className="me-3 btn btn-outline-primary">React(.js)</span>
-                    <span className="me-3 btn btn-outline-primary">Bootstrap</span>
-                    <span className="me-3 btn btn-outline-primary">Material-UI</span>
-                    <span className="me-3 btn btn-outline-primary">HTML</span>
-                    <span className=" btn btn-outline-primary">CSS</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">Javascript</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">React(.js)</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">Bootstrap</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">Material-UI</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">HTML</span>
+                    <span className=" mt-2 btn btn-outline-primary">CSS</span>
                 </p>
             </div>
             <div className="" >
                 <h5 className="text-white">Backend</h5>
                 <p className="text-white">
-                    <span className="me-3 btn btn-outline-primary">Node.js</span>
-                    <span className=" btn btn-outline-primary">express</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">Node.js</span>
+                    <span className="mt-2 btn btn-outline-primary">express</span>
                 </p>
             </div>
             <div className="" >
                 <h5 className="text-white">Database</h5>
                 <p className="text-white">
-                    <span className=" btn btn-outline-primary">Mongodb</span>
+                    <span className="mt-2 btn btn-outline-primary">Mongodb</span>
                 </p>
             </div>
             <div className="">
                 <h5 className="text-white">Tools</h5>
                 <p className="text-white">
-                    <span className="me-3 btn btn-outline-primary">Vscode</span>
-                    <span className="me-3 btn btn-outline-primary">git</span>
-                    <span className="me-3 btn btn-outline-primary">Firebase</span>
-                    <span className="me-3 btn btn-outline-primary">Heroku</span>
-                    <span className=" btn btn-outline-primary">Netlify</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">Vscode</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">git</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">Firebase</span>
+                    <span className="me-3 mt-2 btn btn-outline-primary">Heroku</span>
+                    <span className=" mt-2 btn btn-outline-primary">Netlify</span>
                 </p>
             </div>
 
